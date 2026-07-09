61ef2ab0962a82248b742a626d5bbf509dc3707b
Tabysi <77354592+tabysi@users.noreply.github.com>
2026-07-09 20:38:33 +0200

    docs: update 2026-07-09 18:38

--- content/_meta.js
+++ content/_meta.js
@@ -1,5 +1,6 @@
 export default {
   index: 'Introduction',
+  'lacore-mdt': 'LACORE MDT (Standalone)',
   partners: 'Partners & Creators',
   'frameworks-scripts': 'Frameworks & Scripts',
   anticheat: 'Anti-Cheat & Security',
