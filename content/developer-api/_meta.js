c7e30f4c7538c03f0dff08049c2392855c6808a8
Tabysi <77354592+tabysi@users.noreply.github.com>
2026-07-19 00:30:48 +0200

    docs: update 2026-07-18 22:30

--- content/developer-api/_meta.js
+++ content/developer-api/_meta.js
@@ -1,5 +1,10 @@
 export default {
   index: 'Overview',
+  server: 'Server',
+  client: 'Client',
+  events: 'Events',
+  config: 'Config',
+  examples: 'Examples',
   bridge: 'API Bridge',
   'ai-prompt': 'AI Prompt',
 }
